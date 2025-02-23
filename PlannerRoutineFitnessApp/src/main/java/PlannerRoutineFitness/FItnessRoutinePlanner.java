f4da8f73fd404b27aac82315d53f0a816f963301
Sanjeevini-Jawahar11 <139488402+Sanjeevini-Jawahar11@users.noreply.github.com>
2025-02-23 18:43:09 +0530

    Add files via upload

---
+++ PlannerRoutineFitnessApp/src/main/java/PlannerRoutineFitness/FItnessRoutinePlanner.java
@@ -0,0 +1,55 @@
+package PlannerRoutineFitness;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FItnessRoutinePlanner
+ */
+@WebServlet("/FItnessRoutinePlanner")
+public class FItnessRoutinePlanner extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FItnessRoutinePlanner() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String action=request.getParameter("action");
+		if("CREATE ROUTINE".equals(action)) {
+			response.sendRedirect("CreateRoutine.html");
+		}
+		else if("MY ROUTINE".equals(action)) {
+			response.sendRedirect("My Routine.html");
+		}
+		else if("Contact Us".equals(action)) {
+			response.sendRedirect("Contact Us.html");
+		}
+		response.getContentType();
+//		doGet(request, response);
+	}
+
+}
